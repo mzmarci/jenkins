9a57235579b7c61e63cdae3117bc4e8143dc0cd0
mzmarci <mzmazy100@gmail.com>
2024-03-27 21:20:19 +0100

    updating

--- user_data.sh
+++ user_data.sh
@@ -1,37 +1,26 @@
-#!/bin/bash
-sudo amazon-linux-extras install docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-
-#Make docker auto-start
-sudo chkconfig docker on
-
+ #!/bin/bash 
+sudo yum update -y
 sudo yum install -y git
-
-# Install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo amazon-linux-extras install docker -y
+sudo usermod -a -G docker ec2-user
+sudo chkconfig docker on
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm — import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade
-sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install wget
+sudo amazon-linux-extras install java-openjdk11
+sudo amazon-linux-extras install epel -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum install jenkins -y
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins
-sudo usermod -a -G docker jenkins
-
+sudo service jenkins start
+sudo usermod -aG docker jenkins
+sudo service jenkins restart
 sudo yum install pip -y
 sudo pip install --user ansible
 sudo yum -y python3-pip
 sudo yum install python3 -y
 sudo pip3 install boto3
-sudo yum install https://releases.hashicorp.com/terraform/1.4.6/terraform_1.4.6_linux_amd64.zip"
-unzip terraform_1.4.6_linux_amd64.zip"
-sudo mv terraform /usr/bin"
+sudo yum install https://releases.hashicorp.com/terraform/1.4.6/terraform_1.4.6_linux_amd64.zip
+unzip terraform_1.4.6_linux_amd64.zip
+sudo mv terraform /usr/bin
 sudo yum install git java-11-amazon-corretto-devel.x86_64 -y
-
-#Reboot to verify it all loads fine on its own.
-sudo reboot
